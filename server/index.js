1b47e6cf11aaacb8d8bfc2db06b3784122e06427
brduarte <brunomacielduarte32@gmail.com>
2021-04-17 12:23:59 -0300

    Abstração de factch na api de mockup
    Criando abstração para utilizar em outras funções

--- server/index.js
+++ server/index.js
@@ -1,6 +1,36 @@
+require('dotenv').config()
+const axios = require('axios')
 
 
+function start() {
 
-function start(){
+}
 
+async function getCombinationAirPort() {
+
+    await fetchMockupAirPort('/airports')
+
+}
+
+function fetchMockupAirPort(uri) {
+    try {
+        let api = axios.create({
+            baseURL: `${prosses.env.API_MOCKUP_BASE_URL}/${uri}/${prosses.env.API_MOCKUP_BASE_URL}`,
+            timeout: 1000,
+        })
+
+        api.interceptors.request.use(config => {
+            config.headers = {
+                ...config.headers,
+                auth:{
+                    username: prosses.env.API_MOCKUP_AIRPORT_USERNAME,
+                    password: prosses.env.API_MOCKUP_AIRPORT_PASSWORD
+                }
+            };
+            return config;
+        });
+
+    } catch (error) {
+        console.error(error)
+    }
 }
