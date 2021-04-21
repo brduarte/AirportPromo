dd0ff23b57de0990b725d308c42365a848eac300
brduarte <brunomacielduarte32@gmail.com>
2021-04-20 23:57:09 -0300

    Refatorando script

--- server/index.js
+++ server/index.js
@@ -1,149 +1,51 @@
 import dotenv from 'dotenv'
-import axios from "axios";
 import moment from "moment"
+import {
+    fetchMockupAirPortAPI,
+    getScheduledFlightsByDate,
+    getDistanceFromLatLonInKm,
+    calculateFlightDurationInHours,
+    calculateAverageSpeed,
+    calculatepricePerFare
+} from './utilities/index.js'
 
 dotenv.config()
 
-start()
+flightProcessor()
 
-async function start() {
-
-    const {data} = await fetchMockupAirPortAPI('airports').get()
-    const airports = convertObjectToArray(data)
-    const combinationsAirPorts = await flightProcessor(airports.slice(0, 3))
-    console.log(combinationsAirPorts);
-
-}
-
-
-async function flightProcessor(airports) {
+async function flightProcessor() {
     try {
+        const {data} = await fetchMockupAirPortAPI('airports').get()
+        const airports = Object.values(data).slice(0, 3)
 
-        let result = [];
+        for (const airportA of airports) {
+            for (const airportB of airports) {
 
-        if (airports.length === 0) {
-            result.push([]);
-        } else {
-            for (var i = 0; i < airports.length; i++) {
-                let airportA = airports[i];
+                var {data: scheduledFlights} = await getScheduledFlightsByDate(airportA, airportB, moment().add(40, 'days').format('YYYY-MM-DD'))
+                scheduledFlights.summary.distance = {
+                    value: getDistanceFromLatLonInKm(airportA, airportB),
+                    type: 'Km'
+                }
 
-                for (var j = 0; j < airports.length; j++) {
+                if (airportA.iata === airportB.iata) continue;
 
-                    if (airports[j].iata === airportA.iata) {
-                        continue;
+                scheduledFlights.options.forEach(option => {
+                    let flightHours = calculateFlightDurationInHours(option.departure_time, option.arrival_time)
+
+                    option.average_speed = {
+                        value: calculateAverageSpeed(flightHours, scheduledFlights.summary.distance.value),
+                        type: 'KM/h'
                     }
-
-                    var {data} = await getScheduledFlightsByDate(airportA, airports[j], moment().add(40, 'days').format('YYYY-MM-DD'))
-                    var distanceBetweenAirports = getDistanceFromLatLonInKm(airportA, airports[j])
-
-
-                    data.options.forEach(option => {
-                        let flightHours = calculateFlightDurationInHours(option.departure_time, option.arrival_time)
-
-                        option.average_speed = {
-                            value: calculateAverageSpeed(flightHours, distanceBetweenAirports),
-                            type: 'KM/h'
-                        }
-
-                        option.fare_per_KM = {
-                            value: calculatepricePerFare(option.fare_price, distanceBetweenAirports),
-                            type: 'R$'
-                        }
-                    });
-                }
-
-                data.distance = {
-                    value: distanceBetweenAirports,
-                    type: 'KM'
-                }
-
-                result.push(data)
+                    option.fare_per_KM = {
+                        value: calculatepricePerFare(option.fare_price, scheduledFlights.summary.distance.value),
+                        type: 'R$'
+                    }
+                });
             }
         }
-        return result;
 
     } catch (error) {
-        console.log(error);
+        throw error
     }
-}
-
-async function getScheduledFlightsByDate(airportA, airportB, data) {
-    try {
-        return await fetchMockupAirPortAPI('search').get(`${airportA.iata}/${airportB.iata}/${data}`)
-    } catch (error) {
-        throw error;
-    }
-}
-
-// O sacrificio da performance :(
-function convertObjectToArray(airports) {
-    let result = []
-
-    for (const key in airports) {
-        result.push(airports[key])
-    }
-
-    return result;
-}
-
-function getDistanceFromLatLonInKm(pointA = {lat: '', lon: ''}, pointB = {lat: '', lon: ''}) {
-    const raio = 6371; // Raio da Terra em KM
-    let dLat = deg2rad(pointB.lat - pointA.lat);
-    let dLon = deg2rad(pointB.lat - pointA.lat);
-
-    var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(pointA.lat)) * Math.cos(deg2rad(pointB.lat)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var distancia = raio * c; // Distance in km
-
-    // Converte o número em graus ao equivalente em radianos
-    function deg2rad(deg) {
-        return deg * (Math.PI / 180)
-    }
-
-    return Math.round(distancia);
-}
-
-function fetchMockupAirPortAPI(uri) {
-    try {
-        let api = axios.create({
-            baseURL: `${process.env.API_MOCKUP_BASE_URL}/${uri}/${process.env.API_MOCKUP_AIRPORT_KEY}`,
-        })
-
-        api.interceptors.request.use(config => {
-            config.headers = {
-                ...config.headers,
-            };
-
-            config.auth = {
-                username: process.env.API_MOCKUP_AIRPORT_USERNAME,
-                password: process.env.API_MOCKUP_AIRPORT_PASSWORD
-            }
-
-            return config;
-        });
-
-        return api
-
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-function calculateAverageSpeed(hours, distance) {
-    return Math.round(distance / hours)
-}
-
-function calculatepricePerFare(price, distance) {
-    return (price / distance).toFixed(2)
-}
-
-function calculateFlightDurationInHours(start, end) {
-    let departureTime = moment(start);
-    let arrivalTime = moment(end)
-    const duration = moment.duration(arrivalTime.diff(departureTime));
-    return duration.asHours()
 }
 
