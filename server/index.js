57f2ade5107613b0c46cd713acf02547f58d13bf
brduarte <brunomacielduarte32@gmail.com>
2021-04-22 12:43:22 -0300

    Finalizando parte 1

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv'
 import moment from "moment"
 import {
-    fetchMockupAirPortAPI,
-    getScheduledFlightsByDate,
-    getDistanceFromLatLonInKm,
-    calculateFlightDurationInHours,
-    calculateAverageSpeed,
-    calculatepricePerFare
+  fetchMockupAirPortAPI,
+  getScheduledFlightsByDate,
+  getDistanceFromLatLonInKm,
+  calculateFlightDurationInHours,
+  calculateAverageSpeed,
+  calculatepricePerFare
 } from './utilities/index.js'
 
 dotenv.config()
@@ -14,38 +14,37 @@ dotenv.config()
 flightProcessor()
 
 async function flightProcessor() {
-    try {
-        const {data} = await fetchMockupAirPortAPI('airports').get()
-        const airports = Object.values(data).slice(0, 3)
+  try {
+    const {data} = await fetchMockupAirPortAPI('airports').get()
+    const airports = Object.values(data).slice(0, 3)
 
-        for (const airportA of airports) {
-            for (const airportB of airports) {
+    for (const airportA of airports) {
+      for (const airportB of airports) {
 
-                var {data: scheduledFlights} = await getScheduledFlightsByDate(airportA, airportB, moment().add(40, 'days').format('YYYY-MM-DD'))
-                scheduledFlights.summary.distance = {
-                    value: getDistanceFromLatLonInKm(airportA, airportB),
-                    type: 'Km'
-                }
-
-                if (airportA.iata === airportB.iata) continue;
-
-                scheduledFlights.options.forEach(option => {
-                    let flightHours = calculateFlightDurationInHours(option.departure_time, option.arrival_time)
-
-                    option.average_speed = {
-                        value: calculateAverageSpeed(flightHours, scheduledFlights.summary.distance.value),
-                        type: 'KM/h'
-                    }
-                    option.fare_per_KM = {
-                        value: calculatepricePerFare(option.fare_price, scheduledFlights.summary.distance.value),
-                        type: 'R$'
-                    }
-                });
-            }
+        var {data: scheduledFlights} = await getScheduledFlightsByDate(airportA, airportB, moment().add(40, 'days').format('YYYY-MM-DD'))
+        scheduledFlights.summary.distance = {
+          value: getDistanceFromLatLonInKm(airportA, airportB),
+          type: 'Km'
         }
 
-    } catch (error) {
-        throw error
+        if (airportA.iata === airportB.iata) continue;
+
+        scheduledFlights.options.forEach(option => {
+          let flightHours = calculateFlightDurationInHours(option.departure_time, option.arrival_time)
+
+          option.average_speed = {
+            value: calculateAverageSpeed(flightHours, scheduledFlights.summary.distance.value),
+            type: 'KM/h'
+          }
+          option.fare_per_KM = {
+            value: calculatepricePerFare(option.fare_price, scheduledFlights.summary.distance.value),
+            type: 'R$'
+          }
+        });
+      }
     }
+  } catch (error) {
+    throw error
+  }
 }
 
